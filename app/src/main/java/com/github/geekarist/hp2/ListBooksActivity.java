a730658b90640f97c6b45fec26b3b7f28065cf9d
geekarist <christophe.pele@gmail.com>
2015-12-28 14:20:22 +0100

    Load book cover into image view

--- app/src/main/java/com/github/geekarist/hp2/ListBooksActivity.java
+++ app/src/main/java/com/github/geekarist/hp2/ListBooksActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 
 public class ListBooksActivity extends AppCompatActivity {
 
@@ -14,6 +13,6 @@ public class ListBooksActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_books);
         RecyclerView bookListView = (RecyclerView) findViewById(R.id.book_list_view);
         bookListView.setLayoutManager(new LinearLayoutManager(this));
-        bookListView.setAdapter(new ListBooksAdapter());
+        bookListView.setAdapter(new ListBooksAdapter(this));
     }
 }
--- app/src/main/java/com/github/geekarist/hp2/ListBooksAdapter.java
+++ app/src/main/java/com/github/geekarist/hp2/ListBooksAdapter.java
@@ -1,18 +1,25 @@
 package com.github.geekarist.hp2;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
 public class ListBooksAdapter extends RecyclerView.Adapter<ListBooksAdapter.ViewHolder> {
     private final List<Book> mBooks;
+    private Context mContext;
 
-    public ListBooksAdapter() {
+    public ListBooksAdapter(Context mContext) {
+        this.mContext = mContext;
         mBooks = Arrays.asList(
                 new Book("Henri Potier 1", "isbn", 10, "http://henri-potier.xebia.fr/hp0.jpg"),
                 new Book("Henri Potier 2", "isbn2", 20, "http://henri-potier.xebia.fr/hp1.jpg"));
@@ -29,6 +36,10 @@ public class ListBooksAdapter extends RecyclerView.Adapter<ListBooksAdapter.View
     @Override
     public void onBindViewHolder(ListBooksAdapter.ViewHolder holder, int position) {
         holder.mTitleView.setText(mBooks.get(position).title);
+        Glide.with(mContext)
+                .load(mBooks.get(position).cover)
+                .placeholder(R.drawable.book_cover_placeholder)
+                .into(holder.mImageView);
     }
 
     @Override
@@ -38,10 +49,12 @@ public class ListBooksAdapter extends RecyclerView.Adapter<ListBooksAdapter.View
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView mTitleView;
+        public ImageView mImageView;
 
         public ViewHolder(View itemView) {
             super(itemView);
             this.mTitleView = (TextView) itemView.findViewById(R.id.book_title_view);
+            this.mImageView = (ImageView) itemView.findViewById(R.id.book_image_view);
         }
     }
 }
