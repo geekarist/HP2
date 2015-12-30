0ea837238b132d0883e5c44f9e88e457d54961dd
geekarist <christophe.pele@gmail.com>
2015-12-30 14:18:15 +0100

    Send book as parcelable extra to detail activity

--- app/src/main/java/com/github/geekarist/hp2/Book.java
+++ app/src/main/java/com/github/geekarist/hp2/Book.java
@@ -1,6 +1,9 @@
 package com.github.geekarist.hp2;
 
-public class Book {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Book implements Parcelable {
     final String title;
     final String isbn;
     final int price;
@@ -12,4 +15,36 @@ public class Book {
         this.price = price;
         this.cover = cover;
     }
+
+    protected Book(Parcel in) {
+        title = in.readString();
+        isbn = in.readString();
+        price = in.readInt();
+        cover = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(isbn);
+        dest.writeInt(price);
+        dest.writeString(cover);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
 }
--- app/src/main/java/com/github/geekarist/hp2/BookDetailActivity.java
+++ app/src/main/java/com/github/geekarist/hp2/BookDetailActivity.java
@@ -4,25 +4,26 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.widget.TextView;
 
 public class BookDetailActivity extends Activity {
 
-    public static final String EXTRA_ISBN = "isbn";
+    public static final String EXTRA_BOOK = "book";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_book);
-        TextView detailTitleText = (TextView) findViewById(R.id.book_detail_title_text);
-        detailTitleText.setText(getIntent().getStringExtra(EXTRA_ISBN));
+        TextView bookTitleText = (TextView) findViewById(R.id.book_detail_title_text);
+        Book book = getIntent().getParcelableExtra(EXTRA_BOOK);
+        bookTitleText.setText(book.title);
     }
 
     public static Intent newIntent(Context context, @NonNull Book book) {
         Intent intent = new Intent(context, BookDetailActivity.class);
-        intent.putExtra(EXTRA_ISBN, book.isbn);
+        intent.putExtra(EXTRA_BOOK, book);
         return intent;
     }
 }
