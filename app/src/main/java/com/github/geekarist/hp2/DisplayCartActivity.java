a7464ea3658981122a126dbd068b842ff7f055de
geekarist <christophe.pele@gmail.com>
2016-01-03 20:42:57 +0100

    Add an Up action to CartActivity

--- app/src/main/java/com/github/geekarist/hp2/DisplayCartActivity.java
+++ app/src/main/java/com/github/geekarist/hp2/DisplayCartActivity.java
@@ -2,6 +2,8 @@ package com.github.geekarist.hp2;
 
 import android.content.Intent;
 import android.os.Parcelable;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -22,14 +24,20 @@ public class DisplayCartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_cart);
+
         mCartListView = (RecyclerView) findViewById(R.id.book_cart_view);
         mCartListView.setLayoutManager(new LinearLayoutManager(this));
         ListBooksAdapter adapter = new ListBooksAdapter(this);
         mCartListView.setAdapter(adapter);
         Book book = getIntent().getParcelableExtra(EXTRA_BOOK);
         adapter.addBook(book);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.book_cart_toolbar);
         setSupportActionBar(toolbar);
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     public static Intent newAddToCartIntent(BookDetailActivity bookDetailActivity, Book book) {
