0b086b5cd4f1a37cd98c8d32f0b119cad95b615e
geekarist <christophe.pele@gmail.com>
2016-02-11 07:58:59 +0100

    Fix import: retrofit from presentation: fix test

--- app/src/test/java/com/github/geekarist/hp2/RetrofitOfferCatalogTest.java
+++ app/src/test/java/com/github/geekarist/hp2/RetrofitOfferCatalogTest.java
@@ -1,5 +1,7 @@
 package com.github.geekarist.hp2;
 
+import android.support.annotation.NonNull;
+
 import com.github.geekarist.hp2.data.BookApi;
 import com.github.geekarist.hp2.data.RetrofitOfferCatalog;
 import com.github.geekarist.hp2.domain.Book;
@@ -10,7 +12,7 @@ import com.github.geekarist.hp2.domain.bestoffer.discount.OfferCatalog;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -21,31 +23,36 @@ public class RetrofitOfferCatalogTest {
     @Test
     public void shouldListDiscounts() throws Exception {
         // Given
-//        OfferCatalog offerCatalog = new RetrofitOfferCatalog((joinedIsbnList, callback) ->
-//                callback.onResponse(new BookDiscountCatalog(asList(
-//                        new Offer("slice", 5, 100),
-//                        new Offer("percentage", 5, 0),
-//                        new Offer("minus", 5, 0)))));
+        OfferCatalog offerCatalog = retrofitOfferCatalogReturning(new BookDiscountCatalog(asList(
+                new Offer("slice", 5, 100),
+                new Offer("percentage", 5, 0),
+                new Offer("minus", 5, 0))));
+        List<Book> bookList = Collections.singletonList(new Book("", "", 20, ""));
 
-        OfferCatalog offerCatalog = new RetrofitOfferCatalog(new BookApi() {
+        // When
+        offerCatalog.list(
+                bookList,
+                discounts -> {
+
+                    // Then
+                    assertThat(discounts).containsOnlyElementsOf(
+                            asList(new Offer("slice", 5, 100), new Offer("percentage", 5, 0), new Offer("minus", 5, 0)));
+                },
+                error -> Assertions.fail("Error should not happen"));
+    }
+
+    @NonNull
+    private RetrofitOfferCatalog retrofitOfferCatalogReturning(final BookDiscountCatalog response) {
+        return new RetrofitOfferCatalog(new BookApi() {
             @Override
             public void listCommercialOffers(String joinedIsbnList, BookApiCallback<BookDiscountCatalog> callback) {
-
+                callback.onResponse(response);
             }
 
             @Override
             public void listBooks(BookApiCallback<List<Book>> callback) {
-
             }
         });
-
-        // When
-        offerCatalog.list(new ArrayList<>(), discounts -> {
-
-            // Then
-            assertThat(discounts).containsOnlyElementsOf(
-                    asList(new Offer("slice", 5, 100), new Offer("percentage", 5, 0), new Offer("minus", 5, 0)));
-        }, error -> Assertions.fail("Error should not happen"));
     }
 
 }
